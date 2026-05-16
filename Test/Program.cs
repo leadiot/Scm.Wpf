b1ca6c4d883fa991bc549687f9c9a8a89743cbf2
avenyao <361341288@qq.com>
2026-05-16 19:50:34 +0800

    依赖库更新 。

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,15 @@
-﻿internal class Program
+using Com.Scm.Utils;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        var url = "http://localhost:5000/api/NasSync/Dir";
+        var header = new Dictionary<string, string>
+        {
+            ["appToken"] = "MjA1NTQ0NjM2OTg0MTMxOTkzNjoxNzc4ODkyMTUyMjE5OmI1YjU0Mjg1ZTE2YmRmNWE0ZTRlYjQ2ZmI3YjczMWYy"
+        };
+        var text = HttpUtils.GetString(url, null, header);
+        Console.WriteLine(text);
     }
 }
